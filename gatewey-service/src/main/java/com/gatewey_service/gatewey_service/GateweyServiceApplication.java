a65cfdce147509c3894cf1373c24ae9f2161e0b0
unknown <carlos.taramuel95@gmail.com>
2025-08-21 17:02:52 -0500

    gatewey

---
+++ gatewey-service/src/main/java/com/gatewey_service/gatewey_service/GateweyServiceApplication.java
@@ -0,0 +1,15 @@
+package com.gatewey_service.gatewey_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GateweyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateweyServiceApplication.class, args);
+	}
+
+}
---
+++ gatewey-service/src/test/java/com/gatewey_service/gatewey_service/GateweyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatewey_service.gatewey_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateweyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
