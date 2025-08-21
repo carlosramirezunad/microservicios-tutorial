da8160343d56145fde58a87b754e9f067a760d88
unknown <carlos.taramuel95@gmail.com>
2025-08-21 18:30:35 -0500

    gateway

--- gateway-service/src/main/java/com/gateway_service/gateway_service/GateweyServiceApplication.java
+++ gateway-service/src/main/java/com/gateway_service/gateway_service/GateweyServiceApplication.java
@@ -1,4 +1,4 @@
-package com.gatewey_service.gatewey_service;
+package com.gateway_service.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
