e0cbad2e34afc8941ee0d91f548a7b27457944ea
unknown <carlos.taramuel95@gmail.com>
2025-08-25 15:32:10 -0500

    CB

--- usuario-service/src/main/java/com/usuario/service/usuario_service/controlador/UsuarioController.java
+++ usuario-service/src/main/java/com/usuario/service/usuario_service/controlador/UsuarioController.java
@@ -116,7 +116,7 @@ public class UsuarioController {
         return new ResponseEntity("El usuario : " + id + " no tiene dinero para las motos", HttpStatus.OK);
     }
 
-        private ResponseEntity<List<Carro>> fallBackGetTodos(@PathVariable("usuarioId") int id, RuntimeException exception){
+        private ResponseEntity<Map<String, Object>> fallBackGetTodos(@PathVariable("usuarioId") int id, RuntimeException exception){
         return new ResponseEntity("El usuario : " + id + " tiene los vehiculos en el taller", HttpStatus.OK);
     }
 
